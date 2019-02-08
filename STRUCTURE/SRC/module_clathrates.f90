47931fc4cf115cb3ba118ef926eeb2ab625d11c0
cmmiles <c.miles.1@warwick.ac.uk>
2019-02-08 17:51:55 +0000

    Missing ENDDOs.

--- STRUCTURE/SRC/module_clathrates.f90
+++ STRUCTURE/SRC/module_clathrates.f90
@@ -145,6 +145,8 @@ subroutine compute_f3(F3_atom,first_coord_shell,size_first_coord_shell)
             F3_part = (cos2_num/cos2_den + 0.1111)**2
             ! Add F3/#combinations to total F3
             F3_atom = F3_atom + 2*F3_part/(size_first_coord_shell**2 - size_first_coord_shell)
+        enddo
+    enddo
 
 end subroutine compute_f3
 
