f8137d6888dbf2d73f140ec03b37739377ac6164
cmmiles <c.miles.1@warwick.ac.uk>
2019-02-11 16:12:20 +0000

    Changed output files about again.

--- STRUCTURE/SRC/module_clathrates.f90
+++ STRUCTURE/SRC/module_clathrates.f90
@@ -12,12 +12,15 @@ subroutine clathrates_alloc(switch_f3,switch_f4)
     ! Make the tmp dir and open output files
     !call system("rm -r -f data-c ; mkdir data-c")
     if (trim(adjustl(switch_f3)).eq.'yes') then
-        write(99,*) "We are calculating the clathrate F3 order parameter."
+        write(99,*) "We are calculating the clathrate F3 order parameter. See hin_structure.out.clathrates.stats"
+        open(unit=203, file='hin_structure.out.clathrates.f3.color', status='unknown')
+        write(203,*) "Some sort of F3 order parameter statistics will be written here."
     endif
     if (trim(adjustl(switch_f4)).eq.'yes') then
-        write(99,*) "We are calculating the clathrate F4 order parameter."
+        write(99,*) "We are calculating the clathrate F4 order parameter. See hin_structure.out.clathrates.stats"
+        open(unit=204, file='hin_structure.out.clathrates.f4', status='unknown')
+        write(204,*) "Some sort of F4 order parameter statistics will be written here."
     endif
-    !open(unit=205, file='hin_structure.out.clathrates.color', status='unknown')
     open(unit=206, file='hin_structure.out.clathrates.stats', status='unknown')
     if (trim(adjustl(switch_f3)).eq.'yes') then
         if (trim(adjustl(switch_f4)).eq.'yes') then
