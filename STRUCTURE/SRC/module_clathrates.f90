9bce212df2f65e74e259e1594dae3a4f66fd64bb
cmmiles <c.miles.1@warwick.ac.uk>
2019-02-12 13:37:05 +0000

    Example works now (hopefully)

--- STRUCTURE/SRC/module_clathrates.f90
+++ STRUCTURE/SRC/module_clathrates.f90
@@ -34,7 +34,6 @@ subroutine clathrates_alloc(switch_f3,switch_f4)
 
 end subroutine clathrates_alloc
 
-
 subroutine clathrates(switch_f3,switch_f4,f_zmin,f_zmax,f_cut,n_f_ow,list_f_ow,counter, &
                       time,cart,icell,pos)
 
@@ -49,7 +48,8 @@ subroutine clathrates(switch_f3,switch_f4,f_zmin,f_zmax,f_cut,n_f_ow,list_f_ow,c
     real :: icell(cart*cart)
     integer, allocatable :: list_f_ow(:)    ! Atom indices of OW
     integer :: tot_atoms                    ! Count of number of atoms for which F3 is calculated
-    integer :: first_coord_shell(10,5)      ! First coordination shell of the current atom: (index, dx, dy, dz, dsq)
+    real :: first_coord_shell(10,4)      ! First coordination shell of the current atom: (dx, dy, dz, dsq)
+    integer :: first_coord_shell_ndx(10)    ! First coordination shell atom indices
     integer :: size_first_coord_shell       ! Size of first coordination shell
     real :: F3_atom, F4_atom                ! F3 parameter for triples, atoms
     real :: F3_avg, F4_avg                  ! F3 parameter for frame-wide avg
@@ -65,7 +65,7 @@ subroutine clathrates(switch_f3,switch_f4,f_zmin,f_zmax,f_cut,n_f_ow,list_f_ow,c
             tot_atoms = tot_atoms + 1
             
             ! If atom is in Z-region of interest, calculate it's first coordination shell
-            call compute_clath_coord_shell(i,first_coord_shell,size_first_coord_shell,n_f_ow, &
+            call compute_clath_coord_shell(i,first_coord_shell,first_coord_shell_ndx,size_first_coord_shell,n_f_ow, &
                                            f_zmin,f_zmax,f_cut,cart,icell,counter,pos,list_f_ow)
             
             if (trim(adjustl(switch_f3)).eq.'yes') then
@@ -79,7 +79,8 @@ subroutine clathrates(switch_f3,switch_f4,f_zmin,f_zmax,f_cut,n_f_ow,list_f_ow,c
             endif
             if (trim(adjustl(switch_f4)).eq.'yes') then
                 ! Compute the F4 parameter for the atom
-                call compute_f4(i,F4_atom,first_coord_shell,size_first_coord_shell,cart,icell,pos,list_f_ow)
+                call compute_f4(i,F4_atom,first_coord_shell,first_coord_shell_ndx,size_first_coord_shell, &
+                                cart,icell,pos,list_f_ow)
                 
                 ! Calculate average F4 for the frame (per species)
                 F4_avg = F4_avg + F4_atom
@@ -93,13 +94,13 @@ subroutine clathrates(switch_f3,switch_f4,f_zmin,f_zmax,f_cut,n_f_ow,list_f_ow,c
     
     if (trim(adjustl(switch_f3)).eq.'yes'.and.trim(adjustl(switch_f4)).eq.'yes') then
         ! If we are calculating both order parameters, write to output file
-        write(206,'(1E10.7,2(X,F12.7))') time, F3_avg, F4_avg
+        write(206,'(1E12.6,2(X,F12.7))') time, F3_avg, F4_avg
     else if (trim(adjustl(switch_f3)).eq.'yes') then
         ! If we are calculating only F3, write to output file
-        write(206,'(1E10.7,X,F12.7)') time, F3_avg
+        write(206,'(1E12.6,X,F12.7)') time, F3_avg
     else
         ! If we are calculating only F4, write to output file
-        write(206,'(1E10.7,X,F12.7)') time, F4_avg
+        write(206,'(1E12.6,X,F12.7)') time, F4_avg
     
     endif
 
@@ -110,7 +111,7 @@ end subroutine clathrates
 
 
 ! Computes first coordination shell
-subroutine compute_clath_coord_shell(i,first_coord_shell,size_first_coord_shell,n_f_ow, &
+subroutine compute_clath_coord_shell(i,first_coord_shell,first_coord_shell_ndx,size_first_coord_shell,n_f_ow, &
                                      f_zmin,f_zmax,f_cut,cart,icell,counter,pos,list_f_ow)
 
     implicit none
@@ -119,7 +120,8 @@ subroutine compute_clath_coord_shell(i,first_coord_shell,size_first_coord_shell,
     integer :: n_f_ow                       ! Number of OW atoms
     integer, allocatable :: list_f_ow(:)    ! Atom indices of OW
     integer, allocatable :: tot_atoms(:)    ! Count of number of atoms for which F3 is calculated
-    integer :: first_coord_shell(10,5)      ! First coordination shell of the current atom: (index, dx, dy, dz, dsq)
+    real :: first_coord_shell(10,4)         ! First coordination shell of the current atom: (dx, dy, dz, dsq)
+    integer :: first_coord_shell_ndx(10)    ! First coordination shell atom indices
     integer :: size_first_coord_shell       ! Size of first coordination shell
     real :: dx, dy, dz                      ! X, Y and Z distances between two atoms
     real :: dsq                             ! Square distance between two atoms
@@ -127,7 +129,6 @@ subroutine compute_clath_coord_shell(i,first_coord_shell,size_first_coord_shell,
     real :: f_zmin, f_zmax, f_cut
     real, allocatable :: pos(:,:)
     
-    first_coord_shell(:,:) = 0
     size_first_coord_shell = 0
     do j=1,n_f_ow ! Iterate through other atoms of species
         if (i/=j.and.pos(cart,list_f_ow(j)).ge.(f_zmin-f_cut).and.pos(cart,list_f_ow(j)).le.(f_zmax+f_cut)) then
@@ -144,11 +145,11 @@ subroutine compute_clath_coord_shell(i,first_coord_shell,size_first_coord_shell,
                                 ", at frame ", counter, " exceeds 10 atoms!"
                     EXIT
                 endif
-                first_coord_shell(size_first_coord_shell,1) = list_f_ow(j)
-                first_coord_shell(size_first_coord_shell,2) = dx
-                first_coord_shell(size_first_coord_shell,3) = dy
-                first_coord_shell(size_first_coord_shell,4) = dz
-                first_coord_shell(size_first_coord_shell,5) = dsq
+                first_coord_shell_ndx(size_first_coord_shell) = list_f_ow(j)
+                first_coord_shell(size_first_coord_shell,1) = dx
+                first_coord_shell(size_first_coord_shell,2) = dy
+                first_coord_shell(size_first_coord_shell,3) = dz
+                first_coord_shell(size_first_coord_shell,4) = dsq
             endif
         endif
     enddo
@@ -162,19 +163,19 @@ subroutine compute_f3(F3_atom,first_coord_shell,size_first_coord_shell)
     implicit none
     
     integer :: j, k
-    integer :: first_coord_shell(10,5)      ! First coordination shell of the current atom: (index, dx, dy, dz, dsq)
+    real :: first_coord_shell(10,4)         ! First coordination shell of the current atom: (dx, dy, dz, dsq)
     integer :: size_first_coord_shell       ! Size of first coordination shell
     real :: F3_part, F3_atom                ! F3 parameter for triples, atoms
-    real :: j_dot_k, cos2_num, cos2_den       ! j.k, |cos|cos numerator, |cos|cos denominator
+    real :: j_dot_k, cos2_num, cos2_den     ! j.k, |cos|cos numerator, |cos|cos denominator
 
     F3_atom = 0
     do j=1,size_first_coord_shell-1
         do k=j+1,size_first_coord_shell
-            j_dot_k = first_coord_shell(j,2)*first_coord_shell(k,2) + &
-            first_coord_shell(j,3)*first_coord_shell(k,3) + &
-            first_coord_shell(j,4)*first_coord_shell(k,4)
+            j_dot_k = first_coord_shell(j,1)*first_coord_shell(k,1) + &
+            first_coord_shell(j,2)*first_coord_shell(k,2) + &
+            first_coord_shell(j,3)*first_coord_shell(k,3)
             cos2_num = j_dot_k*abs(j_dot_k)
-            cos2_den = first_coord_shell(j,5)*first_coord_shell(k,5)
+            cos2_den = first_coord_shell(j,4)*first_coord_shell(k,4)
             F3_part = (cos2_num/cos2_den + 0.1111)**2
             ! Add F3/#combinations to total F3
             F3_atom = F3_atom + 2*F3_part/(size_first_coord_shell**2 - size_first_coord_shell)
@@ -185,12 +186,14 @@ end subroutine compute_f3
 
 
 ! Computes F4 order parameter for an atom
-subroutine compute_f4(i,F4_atom,first_coord_shell,size_first_coord_shell,cart,icell,pos,list_f_ow)
+subroutine compute_f4(i,F4_atom,first_coord_shell,first_coord_shell_ndx,size_first_coord_shell, &
+                      cart,icell,pos,list_f_ow)
 
     implicit none
     
     integer :: i, j, cart
-    integer :: first_coord_shell(10,5)      ! First coordination shell of the current atom: (index, dx, dy, dz, dsq)
+    real :: first_coord_shell(10,4)         ! First coordination shell of the current atom: (dx, dy, dz, dsq)
+    integer :: first_coord_shell_ndx(10)    ! First coordination shell indices
     integer :: size_first_coord_shell       ! Size of first coordination shell
     real :: F4_part, F4_atom                ! F3 parameter for triples, atoms
     real, allocatable :: pos(:,:)
@@ -206,13 +209,13 @@ subroutine compute_f4(i,F4_atom,first_coord_shell,size_first_coord_shell,cart,ic
     F4_atom = 0
     do j=1,size_first_coord_shell
         ! Choose Hydrogen from O1. I.e. furthest from O2.
-        dx1 = pos(1,list_f_ow(i)+1)-pos(1,first_coord_shell(j,1))
-        dy1 = pos(2,list_f_ow(i)+1)-pos(2,first_coord_shell(j,1))
-        dz1 = pos(3,list_f_ow(i)+1)-pos(3,first_coord_shell(j,1))
+        dx1 = pos(1,list_f_ow(i)+1)-pos(1,first_coord_shell_ndx(j))
+        dy1 = pos(2,list_f_ow(i)+1)-pos(2,first_coord_shell_ndx(j))
+        dz1 = pos(3,list_f_ow(i)+1)-pos(3,first_coord_shell_ndx(j))
         call images(cart,0,1,1,icell,dx1,dy1,dz1)
-        dx2 = pos(1,list_f_ow(i)+2)-pos(1,first_coord_shell(j,1))
-        dy2 = pos(2,list_f_ow(i)+2)-pos(2,first_coord_shell(j,1))
-        dz2 = pos(3,list_f_ow(i)+2)-pos(3,first_coord_shell(j,1))
+        dx2 = pos(1,list_f_ow(i)+2)-pos(1,first_coord_shell_ndx(j))
+        dy2 = pos(2,list_f_ow(i)+2)-pos(2,first_coord_shell_ndx(j))
+        dz2 = pos(3,list_f_ow(i)+2)-pos(3,first_coord_shell_ndx(j))
         call images(cart,0,1,1,icell,dx2,dy2,dz2)
         if (dx1*dx1+dy1*dy1+dz1*dz1>dx2*dx2+dy2*dy2+dz2*dz2) then
             ! Calculate h1-o1
@@ -229,13 +232,13 @@ subroutine compute_f4(i,F4_atom,first_coord_shell,size_first_coord_shell,cart,ic
         endif
         
         ! Choose Hydrogen from O2. I.e. furthest from O1.
-        dx1 = pos(1,first_coord_shell(j,1)+1)-pos(1,list_f_ow(i))
-        dy1 = pos(2,first_coord_shell(j,1)+1)-pos(2,list_f_ow(i))
-        dz1 = pos(3,first_coord_shell(j,1)+1)-pos(3,list_f_ow(i))
+        dx1 = pos(1,first_coord_shell_ndx(j)+1)-pos(1,list_f_ow(i))
+        dy1 = pos(2,first_coord_shell_ndx(j)+1)-pos(2,list_f_ow(i))
+        dz1 = pos(3,first_coord_shell_ndx(j)+1)-pos(3,list_f_ow(i))
         call images(cart,0,1,1,icell,dx1,dy1,dz1)
-        dx2 = pos(1,first_coord_shell(j,1)+2)-pos(1,list_f_ow(i))
-        dy2 = pos(2,first_coord_shell(j,1)+2)-pos(2,list_f_ow(i))
-        dz2 = pos(3,first_coord_shell(j,1)+2)-pos(3,list_f_ow(i))
+        dx2 = pos(1,first_coord_shell_ndx(j)+2)-pos(1,list_f_ow(i))
+        dy2 = pos(2,first_coord_shell_ndx(j)+2)-pos(2,list_f_ow(i))
+        dz2 = pos(3,first_coord_shell_ndx(j)+2)-pos(3,list_f_ow(i))
         call images(cart,0,1,1,icell,dx2,dy2,dz2)
         if (dx1*dx1+dy1*dy1+dz1*dz1>dx2*dx2+dy2*dy2+dz2*dz2) then
             h2x = dx1
@@ -248,18 +251,18 @@ subroutine compute_f4(i,F4_atom,first_coord_shell,size_first_coord_shell,cart,ic
         endif
         
         ! Calculate lambda, mu
-        h1_dot_o2 = h1x*first_coord_shell(j,2) + h1y*first_coord_shell(j,3) + h1z*first_coord_shell(j,4)
-        h2_dot_o2 = h2x*first_coord_shell(j,2) + h2y*first_coord_shell(j,3) + h2z*first_coord_shell(j,4)
-        lambda = -h1_dot_o2/first_coord_shell(j,5)
-        mu = -h2_dot_o2/first_coord_shell(j,5)
+        h1_dot_o2 = h1x*first_coord_shell(j,1) + h1y*first_coord_shell(j,2) + h1z*first_coord_shell(j,3)
+        h2_dot_o2 = h2x*first_coord_shell(j,1) + h2y*first_coord_shell(j,2) + h2z*first_coord_shell(j,3)
+        lambda = -h1_dot_o2/first_coord_shell(j,4)
+        mu = -h2_dot_o2/first_coord_shell(j,4)
         
         ! Calculate u, v
-        ux = h1x + lambda*first_coord_shell(j,2)
-        uy = h1y + lambda*first_coord_shell(j,3)
-        uz = h1z + lambda*first_coord_shell(j,4)
-        vx = h2x + mu*first_coord_shell(j,2)
-        vy = h2y + mu*first_coord_shell(j,3)
-        vz = h2z + mu*first_coord_shell(j,4)
+        ux = h1x + lambda*first_coord_shell(j,1)
+        uy = h1y + lambda*first_coord_shell(j,2)
+        uz = h1z + lambda*first_coord_shell(j,3)
+        vx = h2x + mu*first_coord_shell(j,1)
+        vy = h2y + mu*first_coord_shell(j,2)
+        vz = h2z + mu*first_coord_shell(j,3)
         
         ! Calculate F4 contribution
         u_dot_v = ux*vx + uy*vy + uz*vz
