28ec70ccf7cd6f9a54a514ea92667a8a8fd3423a
cmmiles <c.miles.1@warwick.ac.uk>
2019-03-07 18:35:34 +0000

    Fixed Fortran syntax

--- STRUCTURE/SRC/module_clathrates.f90
+++ STRUCTURE/SRC/module_clathrates.f90
@@ -86,7 +86,7 @@ subroutine clathrates(switch_f3,switch_f4,f_zmin,f_zmax,f_cut,n_f_ow,list_f_ow,c
                 F3_avg = F3_avg + F3_atom
                 F3_col(list_f_ow(i)) = F3_atom
                 do j=1,f_zbins
-                    if ((pos(cart,list_f_ow(i))<=f_zmax*(j-1)/f_zbins) and (pos(cart,list_f_ow(i))<=f_zmax*j/f_zbins)) then
+                    if ((pos(cart,list_f_ow(i))<=f_zmax*(j-1)/f_zbins) .and. (pos(cart,list_f_ow(i))<=f_zmax*j/f_zbins)) then
                         F3_zbin_len(j) = F3_zbin_len(j) + 1
                         F3_zbin(j,F3_zbin_len(j)) = F3_atom
                     endif
@@ -102,7 +102,7 @@ subroutine clathrates(switch_f3,switch_f4,f_zmin,f_zmax,f_cut,n_f_ow,list_f_ow,c
                 F4_avg = F4_avg + F4_atom
                 F4_col(list_f_ow(i)) = F4_atom
                 do j=1,f_zbins
-                    if ((pos(cart,list_f_ow(i))<=f_zmax*(j-1)/f_zbins) and (pos(cart,list_f_ow(i))<=f_zmax*j/f_zbins)) then
+                    if ((pos(cart,list_f_ow(i))<=f_zmax*(j-1)/f_zbins) .and. (pos(cart,list_f_ow(i))<=f_zmax*j/f_zbins)) then
                         F4_zbin_len(j) = F4_zbin_len(j) + 1
                         F4_zbin(j,F4_zbin_len(j)) = F4_atom
                     endif
