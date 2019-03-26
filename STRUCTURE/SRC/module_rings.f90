6e16b6dfee92fd448d477b5bf32e991a706174eb
cmmiles <c.miles.1@warwick.ac.uk>
2019-03-26 16:08:23 +0000

    Declared missing variable.

--- STRUCTURE/SRC/module_rings.f90
+++ STRUCTURE/SRC/module_rings.f90
@@ -1266,6 +1266,7 @@ subroutine clath_cages(stat_wr,stat_nr,time)
     type(vector3), allocatable :: rings_555(:)
     type(vector3), allocatable :: rings_655(:)
     integer :: n_rings_555, n_rings_655
+    real :: time
     
     integer, dimension(:,:), allocatable :: rings5, rings6
     integer :: nrings5, nrings6
