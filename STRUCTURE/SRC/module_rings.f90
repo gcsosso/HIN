b1039aea4dca293b3dd15d8edd3d375690020ded
cmmiles <c.miles.1@warwick.ac.uk>
2019-04-15 14:01:58 +0100

    Revamped sort_cls.

--- STRUCTURE/SRC/module_rings.f90
+++ STRUCTURE/SRC/module_rings.f90
@@ -1692,6 +1692,9 @@ subroutine sort_cls(clath_clusters,n_clath_clusters,clath_clusters_size,sort_map
                 end do
                 sort_map(j) = i
                 exit
+                if (sort_map(i).eq.0) then
+                    sort_map(i) = i
+                end if
             end if
         end do
     end do
