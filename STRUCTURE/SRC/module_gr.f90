a6aac77cf114bb8c48dcc7d65d83b0c69784bc4b
Matt Warren <mattwarren95@gmail.com>
2020-07-20 12:33:49 +0100

    Created module_gr and module_hydration

---
+++ STRUCTURE/SRC/module_gr.f90
@@ -0,0 +1,17 @@
+module MOD_gr
+
+contains
+
+
+subroutine gr_alloc()
+
+implicit none
+
+end subroutine gr_alloc
+
+
+subroutine gr()
+
+implicit none
+
+end subroutine gr
---
+++ STRUCTURE/SRC/module_hydration.f90
@@ -0,0 +1,17 @@
+module MOD_hydration
+
+contains
+
+
+subroutine hydration_alloc()
+
+implicit none
+
+end subroutine hydration_alloc
+
+
+subroutine hydration()
+
+implicit none
+
+end subroutine hydration
