99a93f0c66c6074fa792db000b0db2d71c64e02c
Matt Warren <mattwarren95@gmail.com>
2020-11-24 17:52:21 +0000

    Updated module_radial to deal with spatial extent issue

--- STRUCTURE/SRC/module_radial.f90
+++ STRUCTURE/SRC/module_radial.f90
@@ -79,17 +79,19 @@ real, allocatable :: pos(:,:), rad(:), rad_norm(:)
 logical(1) :: ws1_mol
 
 ! Local
-integer :: i, j, k, i_spc, j_spc
+integer :: i, j, k, i_spc, j_spc, n_images=27
 integer, allocatable :: rad_sum(:)
 real :: i_pos(3), j_pos(3), xdf, ydf, zdf, r_ij, r2, cell_vol, rad_tmp
-real, allocatable :: rad_dist(:)
+real, allocatable :: rad_dist(:), min_image(:)
 real(8), parameter :: pi=4.0d0*datan(1.0d0), pi4=4.0d0*pi
 
-allocate(rad_sum(rad_bins),rad_dist(n_rad_ws(2)))
+allocate(rad_sum(rad_bins),rad_dist(n_rad_ws(2)),min_image(n_images))
 rad_sum(:)=0
 rad_dist(:)=0.0d0
 rad_tmp=0.0d0
 
+
+! Use for finding minimum distance in minimum image/for anything without spatial extent
 do i=1, n_rad_ws(2) ! First loop over OW
   i_spc=list_rad_ws(2,i)
   i_pos(1)=pos(1,i_spc) ; i_pos(2)=pos(2,i_spc) ; i_pos(3)=pos(3,i_spc)
@@ -119,7 +121,6 @@ do i=1, n_rad_ws(2) ! First loop over OW
     endif
   enddo
 enddo
-
 if (ws1_mol) then ! Binning for cases where ws1 is a molecule/residue/group of atoms
   do i=1, n_rad_ws(2)
     do j=1, rad_bins
@@ -129,6 +130,22 @@ if (ws1_mol) then ! Binning for cases where ws1 is a molecule/residue/group of a
     enddo
   enddo
 endif
+
+! Use for finding minimum distance in all images/for things with spatial extent
+! do i=1,n_rad_ws(2)
+!
+!   i_spc=list_rad_ws(2,i)
+!   min_image(:)=0.0d0
+!   call radial_images(pos,cart,icell,i_spc,list_rad_ws,n_rad_ws(2),min_image)
+!
+!   do j=1,n_images
+!     do k=1,rad_bins
+!       if ((min_image(j).gt.rad(k)-half_dr).and.(min_image(j).le.rad(k)+half_dr)) then
+!         rad_sum(k)=rad_sum(k)+1
+!       endif
+!     enddo
+!   enddo
+! enddo
 
 ! Normalisation
 cell_vol=icell(1)**3.0d0
@@ -145,5 +162,48 @@ do i=1,rad_bins
 enddo
 
 end subroutine radial
+
+subroutine radial_images(pos,cart,icell,i_spc,j_spc_list,j_spc_count,min_image)
+
+! Arguments
+integer :: cart, i_spc, j_spc_count
+integer, allocatable :: j_spc_list(:,:)
+real :: icell(cart*cart)
+real, allocatable :: pos(:,:), min_image(:)
+
+! Local
+integer :: x, y, z, j, image
+real :: i_pos(3), j_pos(3), xdf, ydf, zdf
+real, allocatable :: r_ij(:)
+
+allocate(r_ij(j_spc_count))
+
+i_pos(1)=pos(1,i_spc) ; i_pos(2)=pos(2,i_spc) ; i_pos(3)=pos(3,i_spc) ! position of Ow
+image=1
+
+do x=-1,1
+  do y=-1,1
+    do z=-1,1
+
+      do j=1, j_spc_count ! for all atoms in protein/polymer
+        j_spc=j_spc_list(1,j)
+        j_pos(1)=pos(1,j_spc)+(x*icell(1)) ; j_pos(2)=pos(2,j_spc)+(y*icell(1)) ; j_pos(3)=pos(3,j_spc)+(z*icell(1))
+        xdf=i_pos(1)-j_pos(1) ; ydf=i_pos(2)-j_pos(2) ; zdf=i_pos(3)-j_pos(3)
+        r_ij(j)=sqrt(xdf**2.0d0+ydf**2.0d0+zdf**2.0d0)
+        ! write(*,*) icell(1)*x, icell(1)*y, icell(1)*z
+      enddo
+
+      min_image(image)=minval(r_ij)
+      !write(*,*) image, x, y, z, min_image(image)
+      ! if (min_image(image).lt.0.2d0) then
+      !   write(*,*) image, x, y, z, min_image(image)
+      ! endif
+      image=image+1
+
+    enddo
+  enddo
+enddo
+
+end subroutine radial_images
 
 end module MOD_radial
