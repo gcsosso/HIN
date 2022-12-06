1615acd940841390ea1c1cefa0f27c9579022108
Matt Warren <52166401+matthewtwarren@users.noreply.github.com>
2022-12-06 17:11:05 +0000

    Plotting q3

--- STRUCTURE/PYTHON/hin_structure.plotting.py
+++ STRUCTURE/PYTHON/hin_structure.plotting.py
@@ -18,7 +18,7 @@ from FUNCTIONS.dist import *
 # - Additional informaion for each plotting type: #parser.add_argument("-i","--info", action='store_true', help="Show more information about the plotting parameters for selected plot")
 
 parser = argparse.ArgumentParser(description="Post-processing and plotting tool for HIN order parameters")
-parser.add_argument("parameter", type=str, help="Specify the order parameter to plot", choices=['qt','f3','f4','distance'])
+parser.add_argument("parameter", type=str, help="Specify the order parameter to plot", choices=['qt','q3','f3','f4','distance'])
 
 args = parser.parse_args()
 
@@ -40,23 +40,28 @@ if args.parameter == "qt":
         print("Must select either: Radial/Z distribution [enter 0] ; Probability density [enter 1]")
         exit()
 
-elif (args.parameter == "f3" or args.parameter == "f4"):
-    if args.parameter == "f3":
+elif (args.parameter in ["f3", "f4", "q3"]):
+    if args.parameter == "q3":
         paramLoc=3
+        paramMax=3
+    elif args.parameter == "f3":
+        paramLoc=3
+        paramMax=4
     elif args.parameter == "f4":
         paramLoc=4
+        paramMax=4
     print("\n 0   Radial/Z distribution\n 1   Probability density\n")
     plotType = int(input("Select plot: "))
 
     if plotType == 0:
         fData = fDist()
-        fData.read(paramLoc,4)
-        fData.plot(paramLoc)
+        fData.read(paramLoc,paramMax,args.parameter)
+        fData.plot(paramLoc,args.parameter)
 
     elif plotType == 1:
         qtData = fDens()
-        qtData.read(paramLoc,4)
-        qtData.plot(paramLoc)
+        qtData.read(paramLoc,paramMax,args.parameter)
+        qtData.plot(paramLoc,args.parameter)
 
     else:
         print("Must select either: Radial/Z distribution [enter 0] ; Probability density [enter 1]")
